531c659a07da8c14cb11891b1def07ae1b47db97
AlcirCastro <alcir.figueiredo@icomp.ufam.edu.br>
2024-09-05 19:19:37 -0400

    HelloWorld de bash :)

---
+++ HelloWorld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON="Barney Calhoun"
+
+echo -n "Digite um número: "
+read NUMBER
+
+if [ $NUMBER -gt 0 ]; then
+        echo "O número $NUMBER é maior que zero!"
+elif [ $NUMBER -eq 0 ]; then
+        echo "O número $NUMBER é zero!"
+else
+        echo "O número $NUMBER é menor que zero!"
+fi
